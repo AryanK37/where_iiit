29e5b480b534895fed9b316a16e1869495659d0c
AryanK37 <127508170+AryanK37@users.noreply.github.com>
2025-05-23 20:09:59 +0530

    campus_image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
 export default function Header() {
   return (
-    <div className="header">
-      <div className="logo-text">Where IIIT</div>
-      <hr className="divider" />
-      <p className="subtitle">Upload a campus photo and find out where the photo was taken</p>
-    </div>
+    <>
+      <div className="header">
+        <div className="logo-text">Where IIIT</div>
+        <hr className="divider" />
+        <p className="subtitle">
+          Upload a campus photo and find out where the photo was taken
+        </p>
+      </div>
+
+      <div className="hero-image">
+        <a href="https://www.iiit.ac.in/" target="_blank" rel="noopener noreferrer">
+        <img src="/public/iiit.png" alt="IIIT Campus" />
+        </a>
+      </div>
+    </>
   );
 }
